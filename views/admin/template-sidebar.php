647ad2103296d46cd374f27cf588c39b92b8c82c
We Codify Co <connect@wecodify.co>
2021-05-17 12:29:58 +0530

    updated to 1.1.0

--- views/admin/template-sidebar.php
+++ views/admin/template-sidebar.php
@@ -1,22 +1,22 @@
-<div class="postbox wplyr-support-block">
+<div class="postbox plugin-options-support">
 	<div class="postbox-header">
-		<h3 class="hndle"><?php _e( 'Support', 'wplyr' ); ?></h3>
+		<h3 class="hndle"><?php _e('Support', $plugin_name); ?></h3>
 	</div>
 	<div class="inside">
-		<p>
-			<?php _e( 'Feel free to send us a message if you need help with this plugin or want customization services. We can tackle anything from small fixes to full website development work.', 'wplyr' ); ?>
-		</p>
-		<ul>
-			<li>
+		<div class="support-text">
+            <?php _e('Feel free to send us a message if you need help with this plugin or want customization services. We can tackle anything from small fixes to full website development work.', $plugin_name); ?>
+		</div>
+		<ul class="icon-list">
+			<li class="list-item">
 				<a href="https://wecodify.co/contact/" target="_blank">
 					<span class="dashicons dashicons-editor-help"> </span>
-					<?php _e( 'Contact Plugin Support Team', 'wplyr' ); ?>
+                    <?php _e('Contact Plugin Support Team', $plugin_name); ?>
 				</a>
 			</li>
-			<li>
+			<li class="list-item">
 				<a href="https://www.paypal.me/wecodify/20" target="_blank">
 					<span class="dashicons dashicons-heart"> </span>
-					<?php _e( 'Make A Donation', 'wplyr' ); ?>
+                    <?php _e('Make A Donation', $plugin_name); ?>
 				</a>
 			</li>
 		</ul>
--- views/admin/template-top.php
+++ views/admin/template-top.php
@@ -1,11 +1,11 @@
 <div class="plugin-options-top">
 	<div class="plugin-options-top-image">
-		<img src="<?php echo WPLYR_URL; ?>assets/images/plugin-icon.svg" alt="<?php _e( 'WPlyr Media Block', 'wplyr' ); ?>">
+		<img src="<?php echo WPLYR_URL; ?>assets/images/plugin-icon.svg" alt="<?php _e('WPlyr Media Block', $plugin_name); ?>">
 	</div>
 	<div class="plugin-options-top-title">
 		<h3>
-			<?php _e( 'WPlyr Media Block', 'wplyr' ); ?>
-			<span><?php _e( 'A simple, accessible and customisable media player block for Video, Audio, YouTube and Vimeo.', 'wplyr' ); ?></span>
+            <?php _e('WPlyr Media Block', $plugin_name); ?>
+			<span><?php _e('A simple, accessible and customisable media player block for Video, Audio, YouTube and Vimeo.', $plugin_name); ?></span>
 		</h3>
 	</div>
 </div>
